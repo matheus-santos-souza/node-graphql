5ce30203736fee5dd2ea4d2c79ace95ac4e461ca
matheus-santos-souza <matheus.santos.souza2020@outlook.com>
2024-01-24 22:11:50 -0400

    first commit: Hello World GraphQL

---
+++ src/simple-server.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+    type Query {
+        helloWorld: String!
+    }
+
+    #type Mutation {}
+`
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query: {
+            helloWorld: () => 'Hello World!'
+        }
+    }
+})
+
+server.listen().then(({ url }) => {
+    console.log(`HTTP server running at ${url}`)
+})
