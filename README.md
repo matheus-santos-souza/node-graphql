d920b1ba1603d04bd1fa5bdcbbb644e902462438
matheus-santos-souza <matheus.santos.souza2020@outlook.com>
2024-01-25 21:08:49 -0400

    finish simple graphQL

---
+++ README.md
@@ -0,0 +1,37 @@
+Node.js com TypeScript e GraphQL
+================================
+
+Este projeto é uma aplicação Node.js utilizando TypeScript e GraphQL.
+
+Instalação
+----------
+
+Para instalar as dependências, execute o seguinte comando:
+
+`npm i`
+
+Inicialização do Projeto
+------------------------
+
+Para iniciar o projeto, utilize o seguinte comando:
+
+`npm run dev:start`
+
+Build do projeto
+------------------------
+
+Para dar um build, utilize o seguinte comando:
+
+`npm run build`
+
+Tecnologias Utilizadas
+----------------------
+
+*   Node.js v20.10.0
+*   npm v10.2.3
+*   TypeScript v5.3.3
+*   TSX
+*   TSUP
+*   [GraphQL](https://graphql.org)
+*   [Apollo Server](https://www.apollographql.com/docs)
+*   [typeGraphQL](https://typegraphql.com/)
--- src/simple-server.ts
+++ src/simple-server.ts
@@ -1,18 +1,47 @@
 import { ApolloServer, gql } from "apollo-server";
+import { randomUUID } from "node:crypto";
+
+/** -------Problemas que GraphQL resolvem---------
+* UNDER FETCHING
+* Rota HTTP que retornam poucos dados
+* 
+* OVER FETCHING
+* Rota HTTP que retornam muitos dados
+*/
+
+interface User {
+    id: String,
+    name: String
+}
 
 const typeDefs = gql`
-    type Query {
-        helloWorld: String!
+    type User {
+        id: String!
+        name: String!
     }
 
-    #type Mutation {}
-`
+    type Query {
+        users: [User!]!
+    }
 
+    type Mutation {
+        createUser(name: String!): User!
+    }
+`
+const users: User[] = []
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
         Query: {
-            helloWorld: () => 'Hello World!'
+            users: () => users
+        },
+
+        Mutation: {
+            createUser: (_, args) => {
+                const newUser = { id: randomUUID(), ...args}
+                users.push(newUser);
+                return newUser
+            }
         }
     }
 })
